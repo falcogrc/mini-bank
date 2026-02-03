d8adb9dd0329d0fbee326488d5c0f24d775b3db9
falcogrc <hohlyshev01228@gmail.com>
2026-02-03 16:14:22 +0300

    test: проверка на создание аккаунта и сохранение данных по id

--- src/test/java/com/minibank/controllers/AccountControllerIT.java
+++ src/test/java/com/minibank/controllers/AccountControllerIT.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.awt.*;
 import java.math.BigDecimal;
@@ -58,23 +59,33 @@ public class AccountControllerIT {
 
     @Test
     void shouldReturnAccountById() throws Exception {
-        // TODO: напиши тест:
-        // 1. Создать счет через POST
         CreateAccountRequest request = new CreateAccountRequest();
+
         request.setClientName("Ivan Ivanov");
         request.setInitialBalance(new BigDecimal("1000.00"));
 
-        mockMvc.perform(post("/api/accounts")
+        String responseJson = mockMvc.perform(post("/api/accounts")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
 
-        // 2. Получить его ID из ответа
-        // 3. Вызвать GET /api/accounts/{id}
-        mockMvc.perform(get("/api/accounts/{id}")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
-        // 4. Проверить, что данные совпадают
+        AccountResponse createdAccount = objectMapper.readValue(
+                responseJson,
+                AccountResponse.class
+        );
+
+        System.out.println(createdAccount);
+        Long createdAccountId = createdAccount.getId();
+
+        ResultActions perform = mockMvc.perform(get("/api/accounts/{id}", createdAccountId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(createdAccountId))
+                .andExpect(jsonPath("$.clientName").value("Ivan Ivanov"))
+                .andExpect(jsonPath("$.balance").value(1000.00));
     }
 
     @Test
